fff93df1a047d76300bf0a4be9047aba2556b51e
Amber Dixon <amberhaq@gmail.com>
2013-12-15 20:57:44 -0800

    Handle requests from frontend.

--- filmlocations.py
+++ filmlocations.py
@@ -1,13 +1,40 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask.ext.pymongo import PyMongo
+from flask import render_template
+from bson.json_util import dumps
 
 app = Flask(__name__)
 mongo = PyMongo(app) # assumes default db name = app name
 
-@app.route("/crawlMovieData")
-def crawlMovieData():
-  return jsonify(status='ok')
+class InvalidCoordinateError(Exception):
+  pass
 
+@app.route("/")
+def showFilmLocations():
+  return render_template('index.html')
+
+@app.route("/filmlocations")
+def getFilmLocations():
+  centerLatLng = getLatLng(request.args.get('centerLatLng'))
+  if centerLatLng is None:
+    return ('Must provide valid map center location', 400)
+
+  locations = mongo.db.filmlocations.find({"lngLat": {"$near": {"$geometry": {"type": "Point", "coordinates": centerLatLng}}}},
+                                          {"release_year": 1, "title": 1, "lngLat": 1, "production_company": 1})
+  return dumps({'results': locations})
+
+def getLatLng(coords):
+  if not coords:
+    return None
+  coordArr = coords.split(',')
+  if len(coordArr) != 2:
+    return None
+
+  for l in coordArr:
+    if float(l) == 0.0:
+      return None
+
+  return [float(c) for c in coordArr]
 
 if __name__ == "__main__":
   app.run(debug = True)
